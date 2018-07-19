29b30595e59135ccb5b92b3260237675aeb6d5f3
217868 <arkadiusz.zasina@gmail.com>
2018-07-19 17:31:46 +0200

    Jest error  "Communications link failure", nie mam pojęcia dlaczego. Do tego adres IP jest zmieniony, trzeba wprowadzić dobry

--- app/src/main/java/com/amm/orderify/MainActivity.java
+++ app/src/main/java/com/amm/orderify/MainActivity.java
@@ -6,17 +6,20 @@ import android.view.ActionMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.amm.orderify.helpers.JBDCDriver;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import com.amm.orderify.helpers.JBDCDriver;
+
+
+
 
 
 
 public class MainActivity extends AppCompatActivity {
 
     String tmp;
+    TextView RecordTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,26 +29,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecordTextView = findViewById(R.id.RecordTextView);
+
         try
         {
             JBDCDriver.Initiate();
-            ResultSet results = JBDCDriver.ExecuteStatement("SELECT * FROM tab");
+            //ResultSet results = JBDCDriver.ExecuteStatement("SELECT * FROM tab");
 
+           // tmp=results.getString(0)+" "+ results.getString(1);
 
-            while (results.next())
-            {
-                tmp+=(results.getString(0)+" "+ results.getString(1));
-            }
 
         } catch (SQLException e)
         {
-            e.printStackTrace();
+           e.printStackTrace();
+           RecordTextView.setText(e.getMessage());
         }
 
-        Button GetButton = (Button)findViewById(R.id.GetButton);
+        Button GetButton = findViewById(R.id.GetButton);
         GetButton.setOnClickListener(e ->
         {
-            TextView RecordTextView = (TextView)findViewById(R.id.RecordTextView);
+
 
             RecordTextView.setText(tmp);
 
--- app/src/main/java/com/amm/orderify/helpers/JBDCDriver.java
+++ app/src/main/java/com/amm/orderify/helpers/JBDCDriver.java
@@ -2,15 +2,25 @@ package com.amm.orderify.helpers;
 
 import java.sql.*;
 
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class JBDCDriver
 {
+
     public static Connection myConn;
     public static Statement myStatement;
+    public static final String ipAddress = "192.168.43.14";
 
     public static void Initiate() throws SQLException
     {
-        myConn = DriverManager.getConnection("jdbc:mysql://192.168.1.100:3306/orderify", "root", "1234");
-        myStatement = myConn.createStatement();
+
+
+            myConn = DriverManager.getConnection("jdbc:mysql://192.168.43.14:3306/orderify", "root", "1234");
+            myStatement = myConn.createStatement();
+
     }
     public static ResultSet ExecuteStatement(String query) throws SQLException
     {
