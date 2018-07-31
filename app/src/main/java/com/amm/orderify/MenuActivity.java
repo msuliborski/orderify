853d1209e0b306b7cffb61b75b73a25701ac68f9
217868 <arkadiusz.zasina@gmail.com>
2018-07-31 18:29:24 +0200

    Gównodesign na początek

--- app/src/main/java/com/amm/orderify/MenuActivity.java
+++ app/src/main/java/com/amm/orderify/MenuActivity.java
@@ -3,9 +3,15 @@ package com.amm.orderify;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 public class MenuActivity extends AppCompatActivity
 {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,4 +20,35 @@ public class MenuActivity extends AppCompatActivity
         setContentView(R.layout.activity_menu);
 
     }
+
+    class CustomAdapter extends BaseAdapter
+    {
+
+
+        @Override
+        public int getCount()
+        {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position)
+        {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position)
+        {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            return null;
+        }
+    }
+
+
 }
